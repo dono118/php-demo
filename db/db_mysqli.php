434c1fb8238144e7c9f13cc76be403cdc5d31ef1
dono118 <dono118@126.com>
2020-08-08 11:13:05 +0800

    添加增,删,改等方法

--- db/db_mysqli.php
+++ db/db_mysqli.php
@@ -16,6 +16,7 @@ class Db
         $this->conn = $conn;
     }
 
+    // 查询
     function select($sql)
     {
         $rst = mysqli_query($this->conn, $sql);
@@ -26,6 +27,50 @@ class Db
         return $arr;
     }
 
+    // 增加
+    function add($table, $arr)
+    {
+        $str = $this->arrToStr($arr);
+        $sql = "insert into " . $table . " set " . $str;
+        // echo $sql;
+        // die;
+        return mysqli_query($this->conn, $sql);
+    }
+
+    // 修改
+    function update($table, $arr, $clauz)
+    {
+        $str = $this->arrToStr($arr);
+        $sql = "update " . $table . " set " . $str . " " . $clauz;
+        return mysqli_query($this->conn, $sql);
+    }
+
+    // 删除
+    function delete($table, $clauz)
+    {
+        $sql = "delete from " . $table . " " . $clauz;
+        return mysqli_query($this->conn, $sql);
+    }
+
+    // 数组转字符串
+    // 把数组 array("username" => "lisi", "password" => "123456") 转成 
+    // 字符串 username='lisi',password='123456' 的形式
+    function arrToStr($arr)
+    {
+        $arr1 = array();
+        foreach ($arr as $k => $v) {
+            $arr1[] = $k . "='" . $v . "'";
+        }
+        $str = implode(',', $arr1);
+        return $str;
+    }
+
+    // 增加,删除,修改统一方法
+    function execute($sql)
+    {
+        return mysqli_query($this->conn, $sql);
+    }
+
     function __destruct()
     {
         mysqli_close($this->conn);
@@ -33,5 +78,10 @@ class Db
 }
 
 $db = new Db("test");
-$arr = $db->select("select * from t_user");
-print_r($arr);
+// $arr = $db->select("select * from t_user");
+// print_r($arr);
+// $bool = $db->execute("insert into t_user set username='zhangsan',password='123456'");
+// $bool = $db->add("t_user", array("username" => "lisi", "password" => "123456"));
+// $bool = $db->update("t_user", array("password" => "654321"), "where id=15");
+$bool = $db->delete("t_user", "where id >= 10");
+var_dump($bool);
